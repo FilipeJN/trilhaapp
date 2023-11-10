61009f070a74910f22be18fb8fb992bd2ec75f82
FilipeJN <filipejhosefi@gmail.com>
2023-11-10 17:30:17 -0300

    Adicionando o BottomNavigationBar, trabalhando com a navegação de telas

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -12,6 +12,9 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  PageController controller = PageController(initialPage: 0);
+  int posicaoPagina = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -75,6 +78,12 @@ class _MainPageState extends State<MainPage> {
             children: [
               Expanded(
                 child: PageView(
+                  controller: controller,
+                  onPageChanged: (value) {
+                    setState(() {
+                      posicaoPagina = value;
+                    });
+                  },
                   children: const [
                     Pagina1Page(),
                     Pagina2Page(),
@@ -82,6 +91,19 @@ class _MainPageState extends State<MainPage> {
                   ],
                 ),
               ),
+              BottomNavigationBar(
+                  onTap: (value) {
+                    controller.jumpToPage(value);
+                  },
+                  currentIndex: posicaoPagina,
+                  items: const [
+                    BottomNavigationBarItem(
+                        label: "Pag1", icon: Icon(Icons.home)),
+                    BottomNavigationBarItem(
+                        label: "Pag2", icon: Icon(Icons.add)),
+                    BottomNavigationBarItem(
+                        label: "Pag3", icon: Icon(Icons.person))
+                  ]),
             ],
           )),
     );
