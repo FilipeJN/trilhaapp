7875791d8682c319a3e05fa2502f14b5b981b28e
FilipeJN <filipejhosefi@gmail.com>
2023-11-13 17:37:29 -0300

    Adiconando RadioList e interação com BD(Repository) simulado

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/repositories/nivel_repository.dart';
 import 'package:trilhaapp/shared/widgets/text_label.dart';
 
 class DadosCadastraisPage extends StatefulWidget {
@@ -12,6 +13,15 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
   var nomeController = TextEditingController(text: "");
   var dataNascimentoController = TextEditingController(text: "");
   DateTime? dataNascimento;
+  var nivelRepository = NivelRepository();
+  var niveis = [];
+  var nivelSelecionado = "";
+
+  @override
+  void initState() {
+    niveis = nivelRepository.retornaNiveis();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +37,6 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
             const TextLabel(texto: "Nome"),
             TextField(
               controller: nomeController,
-            ),
-            const SizedBox(
-              height: 10,
             ),
             const TextLabel(texto: "Data de Nascimento"),
             TextField(
@@ -47,6 +54,23 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
                 }
               },
             ),
+            const TextLabel(
+              texto: "Nivel de experiência",
+            ),
+            Column(
+                children: niveis
+                    .map((nivel) => RadioListTile(
+                        dense: true,
+                        title: Text(nivel.toString()),
+                        selected: nivelSelecionado == nivel,
+                        value: nivel.toString(),
+                        groupValue: nivelSelecionado,
+                        onChanged: (value) {
+                          setState(() {
+                            nivelSelecionado = value.toString();
+                          });
+                        }))
+                    .toList()),
             TextButton(onPressed: () {}, child: Text("Salvar")),
           ],
         ),
