f6243fd2cac15ca09416bc6be76154996c1b9f59
FilipeJN <filipejhosefi@gmail.com>
2023-11-13 16:57:58 -0300

    Adicionando DatePicker

--- lib/pages/dados_cadastrais.dart
+++ lib/pages/dados_cadastrais.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:trilhaapp/shared/widgets/text_label.dart';
 
 class DadosCadastraisPage extends StatefulWidget {
   const DadosCadastraisPage({super.key});
@@ -9,6 +10,8 @@ class DadosCadastraisPage extends StatefulWidget {
 
 class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
   var nomeController = TextEditingController(text: "");
+  var dataNascimentoController = TextEditingController(text: "");
+  DateTime? dataNascimento;
 
   @override
   Widget build(BuildContext context) {
@@ -21,12 +24,28 @@ class _DadosCadastraisPageState extends State<DadosCadastraisPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "Nome",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
-            ),
+            const TextLabel(texto: "Nome"),
             TextField(
               controller: nomeController,
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const TextLabel(texto: "Data de Nascimento"),
+            TextField(
+              controller: dataNascimentoController,
+              readOnly: true,
+              onTap: () async {
+                var data = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime(2000, 1, 1),
+                    firstDate: DateTime(1900, 5, 20),
+                    lastDate: DateTime(2023, 10, 23));
+                if (data != null) {
+                  dataNascimentoController.text = data.toString();
+                  dataNascimento = data;
+                }
+              },
             ),
             TextButton(onPressed: () {}, child: Text("Salvar")),
           ],
